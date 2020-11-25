a657a3c18f2d93be732a07b320055eb05a077aeb
Lydia-tech <71572534+Lydia-tech@users.noreply.github.com>
2020-11-25 14:00:08 -0600

    first question prompt

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
- // Notice the lack of parentheses around the `profileDataArr` parameter?
- const profileDataArgs = process.argv.slice(2, process.argv.length);
- const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-      }
-    
-      console.log('================');
-    
-      // Is the same as this...
-      profileDataArr.forEach((profileItem) => {
-        console.log(profileItem)
-      });
-    };
-printProfileData();
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+inquirer
+.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+  }
+])
+ .then(answers => console.log(answers));
+ 
---
+++ src/page-template.js
@@ -0,0 +1,6 @@
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
+  module.exports = generatePage;  
